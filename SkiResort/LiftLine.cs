dcf206152913725b27f634f20251ed4329bc62cf
Jonas Laursen <jona8690@edu.eal.dk>
2017-02-09 21:29:02 +0100

    meh

--- SkiResort/LiftLine.cs
+++ SkiResort/LiftLine.cs
@@ -6,52 +6,7 @@ using System.Threading;
 
 namespace SkiResort {
 	public class LiftLine {
-		public Dictionary<double, Lift> Lifts = new Dictionary<double, Lift>();
-
-		public int PeoplePerLift; // How many can go in 1 lift
-		public TimeSpan TimeInLift; // In ms
-		public TimeSpan TimePerLift; // in MS
-
-		public string Name { get; private set; }
-
-		public Point End { get; private set; } // ID of the Point we're going to.
-		public Point Start { get; private set; } // ID of the  we're going to.
-
-		Thread thisThread;
-		Resort Resort;
-
-		public LiftLine(Resort resort, string name, long destID, long startID, int ppl, TimeSpan til, TimeSpan tpl) {
-			this.Name = name;
-			this.DestinationID = destID;
-			this.StartID = startID;
-
-			this.PeoplePerLift = ppl;
-			this.TimeInLift = til;
-			this.TimePerLift = tpl;
-
-			this.Resort = resort;
-
-			thisThread = new Thread(RunLift);
-			thisThread.Start();
-		}
-
-		public void RunLift() {
-			while (Resort.IsOpen) {
-				int InLift;
-				if (InQue < PeoplePerLift) {
-					InLift = InQue;
-					InQue = 0;
-				} else {
-					InQue = InQue - PeoplePerLift;
-					InLift = PeoplePerLift;
-				}
-
-				InLifts += 25;
-				Lift newLift = new Lift(Resort, this, InLift, TimeInLift, DestinationID);
-				Lifts.Add(newLift.ID, newLift); Resort.Event("Lift created by " + this.Name);
-
-				Thread.Sleep(TimePerLift);
-			}
+		
 		}
 	}
 }
