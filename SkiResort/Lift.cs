4bbaa128fcae93d92e2d043f25d5db1c3a0dda71
Jonas Laursen <jona8690@edu.eal.dk>
2017-02-09 21:18:07 +0100

    Need skiiers

--- SkiResort/Lift.cs
+++ SkiResort/Lift.cs
@@ -10,12 +10,9 @@ namespace SkiResort {
 	class Lift {
 		public int PeoplePerLift; // How many can go in 1 lift
 		public int TimeInLift; // In ms
-		public int TimePerLift; // in MS
 
 		public string Name { get; private set; }
 
 		public Point Destination { get; set; } // Point object for destination
-
-		public int InQue = 0;
 	}
 }
