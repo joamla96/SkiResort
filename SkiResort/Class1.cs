20cf18c8f91c8270d8517adb8680875530193fb9
jona8690 <jona8690@edu.eal.dk>
2017-02-09 12:56:36 +0100

    Add project files.

---
+++ SkiResort/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiResort
+{
+    public class Class1
+    {
+    }
+}
